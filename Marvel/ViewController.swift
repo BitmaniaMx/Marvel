e68f8c779f27fbeb1af7673dbc07832d2172f952
rafaelgonzalez <rafael.glez@gmail.com>
2023-10-03 18:47:14 -0600

    includes: KeyLoader functionality

--- Marvel/ViewController.swift
+++ Marvel/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myKeys : KeyLoader?
+    var myTS = 0.0
+    var myHash = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myKeys = KeyLoader.shared
+        myTS = Date().timeIntervalSince1970
+        print("private key: ", myKeys!.privateKey)
+        print("public key: ", myKeys!.publicKey)
+        print("ts: ", myTS)
+        
+        print("qs:", myKeys!.getQueryString())
+        
     }
-
-
 }
 
